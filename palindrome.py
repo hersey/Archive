c9f0ab628b65bf09c7ba92348787110502d584cd
hersey <hliu.bz@gmail.com>
2014-08-09 14:49:34 -0700

    new python program

---
+++ palindrome.py
@@ -0,0 +1,20 @@
+####This small program is a tool to test whether a word is a palindrome#####
+def first(word):
+	return word[0]
+
+def last(word):
+	return word[-1]
+
+def middle(word):
+	return word[1:-1]
+
+def is_palindrome(word):
+	if len(word)<1:
+		return True
+	if first(word)!=last(word):
+		return False
+	return is_palindrome(middle(word))
+
+
+	###### The exercise is from Thinkpython.com Chapter 6: Fruitful Function #####
+	
